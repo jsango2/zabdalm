f110abe9173d6021a13e4feb0adb75dd5fa9e401
jsango2 <jure.sango@gmail.com>
2021-06-11 22:34:41 +0200

    ispravke

--- src/components/instaDesktop.js
+++ src/components/instaDesktop.js
@@ -2,7 +2,11 @@ import React from "react"
 export const InstaDesktop = () => {
   return (
     <>
-      <div class="powr-social-feed" id="bb984902_1623063813"></div>
+      <div
+        style={{ position: "relative", zIndex: "2" }}
+        class="powr-social-feed"
+        id="bb984902_1623063813"
+      ></div>
       <script src="https://www.powr.io/powr.js?platform=html"></script>
     </>
   )
--- src/components/instaMobile.js
+++ src/components/instaMobile.js
@@ -2,7 +2,11 @@ import React from "react"
 export const InstaMobile = () => {
   return (
     <>
-      <div class="powr-instagram-feed" id="745174e3_1623440574"></div>
+      <div
+        style={{ position: "relative", zIndex: "2" }}
+        class="powr-instagram-feed"
+        id="745174e3_1623440574"
+      ></div>
       <script src="https://www.powr.io/powr.js?platform=react"></script>
     </>
   )
