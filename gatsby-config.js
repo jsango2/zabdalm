9a136d961a3fb7f3cbda1ea7002f56c79f810922
jsango2 <jure.sango@gmail.com>
2021-07-25 21:04:23 +0200

    seo title

--- gatsby-config.js
+++ gatsby-config.js
@@ -227,7 +227,7 @@ module.exports = {
         icon: `content/assets/domicon.png`,
       },
     },
-    // `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
